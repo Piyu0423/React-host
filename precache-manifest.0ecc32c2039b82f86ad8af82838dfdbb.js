69f6c386b393137211b2c46dfa6e63a3db82ff35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-04 10:49:28 +0000

    Deploy Piyu0423/React-host to github.com/Piyu0423/React-host.git:gh-pages

---
+++ precache-manifest.0ecc32c2039b82f86ad8af82838dfdbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2336f44604271e107cc6c5a1d6bac809",
+    "url": "/React-host/index.html"
+  },
+  {
+    "revision": "312c8479b17d1c60f99a",
+    "url": "/React-host/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "531cb643d43699c87045",
+    "url": "/React-host/static/js/2.54ae325a.chunk.js"
+  },
+  {
+    "revision": "312c8479b17d1c60f99a",
+    "url": "/React-host/static/js/main.f06f4c72.chunk.js"
+  },
+  {
+    "revision": "6c6dd4df53026434b1a8",
+    "url": "/React-host/static/js/runtime-main.e1856f9e.js"
+  }
+]);
